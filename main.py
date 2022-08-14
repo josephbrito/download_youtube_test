209927b23c9f826d3045547ff28259f664499737
= <=>
2022-08-13 21:25:44 -0300

    adding PySimpleGUI - no 100% complete

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from __future__ import unicode_literals
 import youtube_dl
+import PySimpleGUI as sg
 
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=hT_nvWreIhg'])
+layout = [
+    [sg.Text("Insira a URL", size=(12, 1)), sg.InputText()],
+    [sg.Button("Baixar")]
+]
+
+window = sg.Window("Made by José de Brito", layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+    ydl_opts = {
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([values[0]])
+        layout.append([sg.Text("Baixando...")])
+
